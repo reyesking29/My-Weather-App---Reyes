e60807b01e4d52effbe198e3450c0929950478c9
reyesking29 <reyesrob29@gmail.com>
2024-09-16 14:26:40 -0700

    changed default city to LA

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
 
+  cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
@@ -15,10 +17,11 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Los Angeles");
