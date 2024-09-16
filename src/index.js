487bb8a925ec28c09c98142e81b694c761705041
reyesking29 <reyesrob29@gmail.com>
2024-09-16 13:52:49 -0700

    refresh weather function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+function refreshWeather(response) {
+  let valueElement = document.querySelector("#value");
+
+  valueElement.innerHTML = Math.round(value);
+}
+
+function searchCity(city) {
+  // make api call and update the interface
+  let apiKey = "a6fa43b1789f324t7dff7bf43co046f3";
+  let apiURL =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${api}";
+  axios.get(apiURL).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
